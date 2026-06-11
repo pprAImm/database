179dff7fd6254e4b1eb2bfc2810fa3522721784d
cy4strii <arinasushko@yandex.ru>
2026-06-11 16:29:02 +0300

    fix rating

--- sql/queries/series.sql
+++ sql/queries/series.sql
@@ -1,19 +1,19 @@
 -- name: GetSeriesByCategory :many
-SELECT id, title, description, cover_url, rating
+SELECT id, title, description, cover_url
 FROM series
 WHERE category_id = $1;
 
 -- name: GetSeriesByID :one
-SELECT id, title, description, cover_url, rating
+SELECT id, title, description, cover_url
 FROM series
 WHERE id = $1;
 
 -- name: SearchSeries :many
-SELECT id, title, description, cover_url, rating
+SELECT id, title, description, cover_url
 FROM series
 WHERE title ILIKE '%' || $1 || '%';
 
 -- name: CreateSeries :one
-INSERT INTO series (title, description, category_id, cover_url, rating)
+INSERT INTO series (title, description, category_id, cover_url)
 VALUES ($1, $2, $3, $4, $5)
 RETURNING *;
