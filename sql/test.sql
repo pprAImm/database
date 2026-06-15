491d2ba62fb3a28c2c380c5e17624693c4ad9e7f
justchelovk <misha.freidson@gmail.com>
2026-06-15 13:22:57 +0300

    обновление для серверной дб

--- sql/test.sql
+++ sql/test.sql
@@ -1,14 +1,22 @@
+-- Очистка таблиц (в правильном порядке из-за foreign keys)
+TRUNCATE episodes CASCADE;
+TRUNCATE series CASCADE;
+TRUNCATE categories CASCADE;
+
+-- Вставка категорий
 INSERT INTO categories (name, slug) VALUES
     ('Аниме', 'anime'),
     ('Документальное', 'docs'),
     ('Комедия', 'comedy');
 
-INSERT INTO series (title, description, category_id, cover_url, year, rating) VALUES
-    ('фруктовый хаус', 'школа соцсеть', 1, 'https://picsum.photos/300/400?random=1', 2024, 8.5),
-    ('перерождение императрицы', 'я клубника ты клубника как мог родиться банан', 1, 'https://picsum.photos/300/400?random=2', 2024, 7.9),
-    ('тун тун тун сахур', 'сикас севен', 2, 'https://picsum.photos/300/400?random=3', 2024, 9.1);
+-- Вставка сериалов
+INSERT INTO series (title, description, category_id, cover_url) VALUES
+    ('фруктовый хаус', 'школа соцсеть', (SELECT id FROM categories WHERE slug = 'anime'), 'https://picsum.photos/300/400?random=1'),
+    ('перерождение императрицы', 'я клубника ты клубника как мог родиться банан', (SELECT id FROM categories WHERE slug = 'anime'), 'https://picsum.photos/300/400?random=2'),
+    ('тун тун тун сахур', 'сикас севен', (SELECT id FROM categories WHERE slug = 'docs'), 'https://picsum.photos/300/400?random=3');
 
+-- Вставка эпизодов
 INSERT INTO episodes (series_id, title, tiktok_url, episode_num) VALUES
-    (1, 'Пробуждение', 'https://www.tiktok.com/@example/video/1', 1),
-    (1, 'Первая битва', 'https://www.tiktok.com/@example/video/2', 2),
-    (2, 'Сон первый', 'https://www.tiktok.com/@example/video/3', 1);
+    ((SELECT id FROM series WHERE title = 'фруктовый хаус'), 'Пробуждение', 'https://www.tiktok.com/@example/video/1', 1),
+    ((SELECT id FROM series WHERE title = 'фруктовый хаус'), 'Первая битва', 'https://www.tiktok.com/@example/video/2', 2),
+    ((SELECT id FROM series WHERE title = 'перерождение императрицы'), 'Сон первый', 'https://www.tiktok.com/@example/video/3', 1);
